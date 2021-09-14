9d7515627fb92753760ff5061cd66a91146878b0
miguel <i.s.mavs1223@gmail.com>
2021-09-13 23:50:23 -0500

    actualizando

--- js/main.js
+++ js/main.js
@@ -58,3 +58,21 @@ const mixer = mixitup('.portafolio__container', {
       }
   }
   linkPortafolio.forEach(n => n.addEventListener('click', activePortafolio))
+
+
+
+
+
+  /* input formulario*/ 
+
+  const $form = document.querySelector('#form')
+  const $buttonMailto = document.querySelector('#trucazo')
+
+  $form.addEventListener('submit', handleSubmit)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:me@leonidasesteban.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+    }
